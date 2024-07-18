113815ad8784e80814abaa37cbba8057e88c3c0c
jbowerjr <jerry.bower@bowersonservices.com>
2024-07-18 06:18:55 -0500

    Got the search bar to work. Added tooltips for all features.

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 
 (() => {
-	let layerSel, prev_bounds, selectedFtr,	colorFn, $select;
-	let data_id = 'num_apd_he';
-	let data_cat = 'state';
+	let layerSel, prevBounds, selectedFtr,	colorFn, $select;
+	let dataId = 'num_apd_he';
+	let dataCat = 'state';
 	let ftrLayers = {'state': null, 'airport': null};
 	let ftrGeo = {'state': null, 'airport': null};
-	let panel_open = false;
+	let panelOpen = false;
+	let bh = {};
+	let placeholder = {
+		state: 'Search by state name or USPS abbreviation',
+		airport: 'Search by airport name or FAA location identifier'
+	}
+	const ta = $('.typeahead');
 	// Create map
+	const mapBounds = L.latLngBounds(L.latLng(50, -61), L.latLng(19, -127));
 	const map = L.map('map-container', {renderer: L.canvas(), preferCanvas: true})
 		.setView([39.8283, -98.5795], 4)
-		.setMaxBounds(L.latLngBounds(L.latLng(50, -61), L.latLng(19, -127)));
+		.setMaxBounds(mapBounds);
 	// Add attribution
 	map.attributionControl.addAttribution('Map created by <A HREF="https://www.indecon.com/" TARGET="_blank">Industrial Economics Incorporated</A>');
 	map.attributionControl.addAttribution('Powered by <A HREF="http://esri.maps.arcgis.com/home/index.html" TARGET="_blank">Esri</A>');
@@ -38,28 +45,74 @@
 		        mouseout: _resetHighlight,
 		        click: _selectFeatureFromMap
 		    });
+				layer.bindTooltip(feature.properties['NAME']);
 			}
 		});
-		if (!!ftrLayers['airport']) _activateLayer();
+
+		const states = ftrGeo['state'].features.map(
+			function(o) {
+				const props = o.properties;
+				return {
+					id: props['STATEFP'],
+					text: props['NAME'] + ' ' + props['STUSPS'],
+					label: props['NAME'] + ' (' + props['STUSPS'] + ')',
+					ftr: o
+				};
+			}
+		);
+
+		bh['state'] = new Bloodhound({
+			local: states,
+			queryTokenizer: Bloodhound.tokenizers.whitespace,
+			datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+			identify: function(obj) { return obj.id; },
+			sorter: function(a,b){
+				return a.label.toUpperCase() < b.label.toUpperCase() ? -1 : 1;
+			}
+		});
+		_completeInit();
 	});
 
 	// Create the airport feature group layer
 	$.getJSON('data/FAA_Top_200_Airports.json', function(data) {
-			ftrGeo['airport'] = topojson.feature(data, data.objects.FAA_Top_200_Airports);
-			ftrLayers['airport'] = L.geoJson(ftrGeo['airport'], {
-				style: _ftrStyle,
-				pointToLayer: function (feature, latlng) {
-					return L.circleMarker(latlng, styles['airport']);
-				},
-				onEachFeature: function(feature, layer) {
-					layer.on({
-						mouseover: _highlightFeature,
-						mouseout: _resetHighlight,
-						click: _selectFeatureFromMap
-					});
-				}
-			});
-		if (!!ftrLayers['state']) _activateLayer();
+		ftrGeo['airport'] = topojson.feature(data, data.objects.FAA_Top_200_Airports);
+		ftrLayers['airport'] = L.geoJson(ftrGeo['airport'], {
+			style: _ftrStyle,
+			pointToLayer: function (feature, latlng) {
+				return L.circleMarker(latlng, styles['airport']);
+			},
+			onEachFeature: function(feature, layer) {
+				layer.on({
+					mouseover: _highlightFeature,
+					mouseout: _resetHighlight,
+					click: _selectFeatureFromMap
+				});
+				layer.bindTooltip(feature.properties['NAME'] + ' (' + feature.properties['LOC_ID'] + ')');
+			}
+		});
+
+		const airports = ftrGeo['airport'].features.map(
+			function(o) {
+				const props = o.properties;
+				return {
+					id: props['UNIQUE_ID'],
+					text: props['NAME'] + ' ' + props['LOC_ID'],
+					label: props['NAME'] + ' (' + props['LOC_ID'] + ')',
+					ftr: o
+				};
+			}
+		);
+
+		bh['airport'] = new Bloodhound({
+			local: airports,
+			queryTokenizer: Bloodhound.tokenizers.whitespace,
+			datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+			identify: function(obj) { return obj.id; },
+			sorter: function(a,b){
+				return a.label.toUpperCase() < b.label.toUpperCase() ? -1 : 1;
+			}
+		});
+		_completeInit();
 	});
 
 	// Build the slide reveal
@@ -72,34 +125,49 @@
 		hide: function(p, o) { p.removeClass('panel-shadow'); },
 		hidden: _closePanel,
 		show: function(p, o) { p.addClass('panel-shadow'); },
-		shown: function() { panel_open = true;}
+		shown: function() { panelOpen = true;}
 	});
 	
 	$('#info-panel .close-button').on('click', function(){$('#info-panel').slideReveal('hide');})
 	$('#layer-switch').on('click', () => _activateLayer(true));
-	
-	// _initTypeahead();
+	$('#tt-clear').on('click', _clearTypeahead);
 
+
+	function _completeInit() {
+		if (!!ftrLayers['airport'] && !!ftrLayers['state']) {
+			_activateLayer();
+			_initTypeahead();
+		}
+	}
 
 	function _activateLayer(toggle) {
 		if (!!toggle) {
-			if (panel_open) {
+			if (panelOpen) {
+				// We want to zoom back out to the whole map
+				prevBounds = mapBounds;
 				$('#info-panel').slideReveal('hide');
+			} else {
+				// We want to zoom back out to the whole map
+				map.flyToBounds(mapBounds);
 			}
 			// Remove the old feature group layer
-			ftrLayers[data_cat].remove();
-			data_cat = data_cat === 'state' ? 'airport' : 'state';
+			ftrLayers[dataCat].remove();
+			dataCat = dataCat === 'state' ? 'airport' : 'state';
 			// Recalculate the data color ranges
 			$select.change();
+			// Reinit the typeahead
+			_clearTypeahead();
+			ta.typeahead('destroy');
+			_initTypeahead();
 		}
 		// Activate the feature layer
-		ftrLayers[data_cat].addTo(map);
+		ftrLayers[dataCat].addTo(map);
 	}
 
 	function _updateLegend() {
 		const div = L.DomUtil.create('div', 'map-ctrl legend');
 		const scale = colorFn.classes();
-		const option = data_config.find(function(o){return o.ref_id === data_id});
+		const option = data_config.find(function(o){return o.ref_id === dataId});
 		let txt, lowval, highval;
 		for (let x = 0, y = scale.length - 1; x<y; x++) {
 			lowval = x === 0 ? '' : __formatVal(scale[x]);
@@ -118,8 +186,8 @@
 	};
 	
 	function _setChromaRanges() {
-		const data = iecData[data_cat][data_id];
-		const option = data_config.find(function(o){return o.ref_id === data_id});
+		const data = iecData[dataCat][dataId];
+		const option = data_config.find(function(o){return o.ref_id === dataId});
 		let vals = [];
 		let ranges = [];
 		for (key in data) {
@@ -140,11 +208,11 @@
 	}	
 	
 	function _ftrStyle(feature) {
-		return Object.assign({}, styles[data_cat],{fillColor: _getColor(feature.properties.UNIQUE_ID || feature.properties.STUSPS)});
+		return Object.assign({}, styles[dataCat],{fillColor: _getColor(feature.properties.UNIQUE_ID || feature.properties.STUSPS)});
 	}
 
 	function _getColor(id) {
-		const  data = iecData[data_cat][data_id];
+		const  data = iecData[dataCat][dataId];
 		return colorFn(data[id]).hex();
 	}	
 	
@@ -155,7 +223,7 @@
 	}
 
 	function _resetHighlight(e) {
-		ftrLayers[data_cat].resetStyle();
+		ftrLayers[dataCat].resetStyle();
 	}
 
 	function _selectFeatureFromMap(e) {
@@ -166,12 +234,12 @@
     selectedFtr = ftr;
     const id = ftr.properties.UNIQUE_ID || ftr.properties.STUSPS;
     // If the side panel hasn't been opened then store the current bounds
-    if (!panel_open) prev_bounds = map.getBounds();
+    if (!panelOpen) prevBounds = map.getBounds();
 		// Remove the previous selected layer if one exists
 		if (!!layerSel) layerSel.remove();
 		// Now create a new layer from the geometry
 		layerSel = L.GeoJSON.geometryToLayer(ftr.geometry);
-		if (data_cat === 'state') {
+		if (dataCat === 'state') {
 			layerSel.setStyle(Object.assign({}, styles['selected'], {fillColor: _getColor(id)}));
 			layerSel.addTo(map);
 			// Bring it to the front so it stays visible
@@ -193,104 +261,60 @@
 			}
 		);
 		$select.on('change', function() {
-			data_id = this.value;
+			dataId = this.value;
 			_setChromaRanges();
-			ftrLayers[data_cat].clearLayers();
-			ftrLayers[data_cat].addData(ftrGeo[data_cat]);
+			ftrLayers[dataCat].clearLayers();
+			ftrLayers[dataCat].addData(ftrGeo[dataCat]);
 		});
 		
 	}
 	
 	function _initTypeahead() {
-		// Create an array of the counties with State Abbreviations appended (for duplicate names)
-		// var counties = counties_geojson.features.map(
-		// 	function(o) {
-		// 		var props = o.properties;
-		// 		var state_id = props.GEOID.slice(0,2);
-		// 		var state_abbrev = state_codes[state_id].abbrev;
-		// 		var cty_name = _fullCountyTitle(o) + ' (' + state_abbrev + ')';
-		// 		return {'text': cty_name, 'feature': o};
-		// 	}
-		// );
-
-		var bh_engine = new Bloodhound({
-		    local: counties,
-		    queryTokenizer: Bloodhound.tokenizers.whitespace,
-		    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
-		    sufficient: Infinity,
-		    remote: {
-		        url: arcgis_sug_url,
-		        wildcard: '%TEXT',
-		        rateLimitWait: 500,
-		        transform: function(o) {		        	
-		        	return bh_engine.sorter(o.suggestions);
-		        }
-	    	},
-	    	sorter: function(a,b){
-	    		return a.text.toUpperCase() < b.text.toUpperCase() ? -1 : 1;
-	    	}
-		});
-				
-		$('.typeahead').typeahead({
+		ta.typeahead({
 			  hint: true,
 			  highlight: true,
-			  minLength: 4			  
+			  minLength: 2
 			}, {
-			name: 'ctys',
-			display: 'text',
-			source: bh_engine,
+			display: 'label',
+			source: bh[dataCat],
 			async: true,
 			limit: Infinity
 		}).bind('typeahead:select', function(ev, suggestion) {
-			if (!!suggestion.feature) {
-				// This is a county from our data
-				_selectFeature(suggestion.feature);
-			} else {
-				// This is an item from arcgis suggest
-				var params = "SingleLine=" + suggestion.text + "&magicKey=" + suggestion.magicKey;
-				$.get(arcgis_find_url + params, function(data) {
-					var loc = data.candidates[0].location;
-					var results = leafletPip.pointInLayer([loc.x, loc.y], layerCounty, true);
-					if (results.length > 0) {
-						_selectFeature(results[0].feature);
-					}
-				});
-			};
+			_selectFeature(suggestion.ftr);
 			// Remove focus from the input field or ESC won't close the info panel
 			$('.tt-input').blur();
 		});
-		
+
+		ta.attr('placeholder', placeholder[dataCat]);
+	}
+
+	function _clearTypeahead() {
+		ta.typeahead('val', '');
 	}
 	
 	function _closePanel() {
-		panel_open = false;
+		panelOpen = false;
 		// Remove the selected layer
 		layerSel.remove();
-		// Fly back to the previous bounds
-		map.flyToBounds(prev_bounds);
+		// Fly back to the previous bounds or map bounds
+		map.flyToBounds(prevBounds);
 	}
 	
 	function _displayInfoPanel() {
 		const id = selectedFtr.properties.UNIQUE_ID || selectedFtr.properties.STUSPS;
-		const title = (data_cat === 'state' ? 'State of ' : '') + selectedFtr.properties['NAME'];
+		const title = (dataCat === 'state' ? 'State of ' : '') + selectedFtr.properties['NAME'];
 		$('.data-container .header').text(title);
 		data_config.forEach(function(o){
 			$('.data-' + o.ref_id).text(map.name);
-			const tmp = iecData[data_cat][o.ref_id];
+			const tmp = iecData[dataCat][o.ref_id];
 			const val = o.currency ? '$' + ((tmp[id]/1000).toFixed()*1000).toLocaleString() : tmp[id].toFixed(tmp[id] >= 10 ? 0 : 1);
 			$('.data-' + o.ref_id).text(val);
 		});
 		
 		// Open the side panel
-		if(! panel_open) {
+		if(! panelOpen) {
 			infoPanel.slideReveal('show');
 		}
-	}
-	
-	function _fullCountyTitle(ftr){
-		var name = ftr.properties.NAME
-		var lsad = ftr.properties.LSAD;
-		return name + (lsad=='00' ? '' : ' ' + lsad_codes[lsad]);
 	}
 	
 })();
--- options.js
+++ options.js
@@ -1,5 +1,3 @@
-const arcgis_sug_url = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?f=json&category=Address,Postal&countryCode=USA&maxSuggestions=15&searchExtent=-127,19,-61,50&text=%TEXT';
-const arcgis_find_url = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?forStorage=false&f=json&';
 const scale_color_count = 5;
 const data_config = [
 	{
@@ -80,71 +78,3 @@ const styles = {
     }
 }
 
-
-const state_codes = {
-		'01': {'abbrev':'AL', name: 'Alabama'},
-		'02': {'abbrev':'AK', name: 'Alaska'},
-		'04': {'abbrev':'AZ', name: 'Arizona'},
-		'05': {'abbrev':'AR', name: 'Arkansas'},
-		'06': {'abbrev':'CA', name: 'California'},
-		'08': {'abbrev':'CO', name: 'Colorado'},
-		'09': {'abbrev':'CT', name: 'Connecticut'},
-		'10': {'abbrev':'DE', name: 'Delaware'},
-		'11': {'abbrev':'DC', name: 'District of Columbia'},
-		'12': {'abbrev':'FL', name: 'Florida'},
-		'13': {'abbrev':'GA', name: 'Georgia'},
-		'15': {'abbrev':'HI', name: 'Hawaii'},
-		'16': {'abbrev':'ID', name: 'Idaho'},
-		'17': {'abbrev':'IL', name: 'Illinois'},
-		'18': {'abbrev':'IN', name: 'Indiana'},
-		'19': {'abbrev':'IA', name: 'Iowa'},
-		'20': {'abbrev':'KS', name: 'Kansas'},
-		'21': {'abbrev':'KY', name: 'Kentucky'},
-		'22': {'abbrev':'LA', name: 'Louisiana'},
-		'23': {'abbrev':'ME', name: 'Maine'},
-		'24': {'abbrev':'MD', name: 'Maryland'},
-		'25': {'abbrev':'MA', name: 'Massachusetts'},
-		'26': {'abbrev':'MI', name: 'Michigan'},
-		'27': {'abbrev':'MN', name: 'Minnesota'},
-		'28': {'abbrev':'MS', name: 'Mississippi'},
-		'29': {'abbrev':'MO', name: 'Missouri'},
-		'30': {'abbrev':'MT', name: 'Montana'},
-		'31': {'abbrev':'NE', name: 'Nebraska'},
-		'32': {'abbrev':'NV', name: 'Nevada'},
-		'33': {'abbrev':'NH', name: 'New Hampshire'},
-		'34': {'abbrev':'NJ', name: 'New Jersey'},
-		'35': {'abbrev':'NM', name: 'New Mexico'},
-		'36': {'abbrev':'NY', name: 'New York'},
-		'37': {'abbrev':'NC', name: 'North Carolina'},
-		'38': {'abbrev':'ND', name: 'North Dakota'},
-		'39': {'abbrev':'OH', name: 'Ohio'},
-		'40': {'abbrev':'OK', name: 'Oklahoma'},
-		'41': {'abbrev':'OR', name: 'Oregon'},
-		'42': {'abbrev':'PA', name: 'Pennsylvania'},
-		'44': {'abbrev':'RI', name: 'Rhode Island'},
-		'45': {'abbrev':'SC', name: 'South Carolina'},
-		'46': {'abbrev':'SD', name: 'South Dakota'},
-		'47': {'abbrev':'TN', name: 'Tennessee'},
-		'48': {'abbrev':'TX', name: 'Texas'},
-		'49': {'abbrev':'UT', name: 'Utah'},
-		'50': {'abbrev':'VT', name: 'Vermont'},
-		'51': {'abbrev':'VA', name: 'Virginia'},
-		'53': {'abbrev':'WA', name: 'Washington'},
-		'54': {'abbrev':'WV', name: 'West Virginia'},
-		'55': {'abbrev':'WI', name: 'Wisconsin'},
-		'56': {'abbrev':'WY', name: 'Wyoming'},
-		'60': {'abbrev':'AS', name: 'American Samoa'},
-		'66': {'abbrev':'GU', name: 'Guam'},
-		'69': {'abbrev':'MP', name: 'Northern Mariana Islands'},
-		'72': {'abbrev':'PR', name: 'Puerto Rico'},
-		'74': {'abbrev':'UM', name: 'U.S. Minor Outlying Islands'},
-		'78': {'abbrev':'VI', name: 'U.S. Virgin Islands'}
-}
-
-const lsad_codes = {
-		'00': '',
-		'06': 'County',
-		'12': 'Municipality',
-		'15': 'Parish',
-		'25': 'City'
-}
